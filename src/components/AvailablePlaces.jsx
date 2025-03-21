a7b896082a9ea5f486fe3b7ccea748beaf05efb5
Suhas Bachhav <ksb.suhas@gmail.com>
2025-03-21 20:12:15 +0530

    api call via promise

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,10 +1,23 @@
+import { useEffect, useState } from 'react';
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+
+  const [availablePlaces, setAvaialablePlaces] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/places").then((response)=>{
+      return response.json();
+    }).then((data)=>{
+      setAvaialablePlaces(data.places);
+    });
+  }, []);
+
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
